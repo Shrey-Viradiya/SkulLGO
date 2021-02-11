d0ecff0e67730844ed28f7d6a8aa6d381d02374c
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2021-02-11 21:30:21 +0530

    AVL changes 💀☠

--- Prac2_AVL.cpp
+++ Prac2_AVL.cpp
@@ -7,20 +7,25 @@
 int main(){
     AVL data("Ajino Motado");
     
-    data.AddData("data.csv", 1);
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     data.insert(i,i*10);
-    // }
+    // data.AddData("data.csv", 1);
+    for (int i = 0; i < 10; i++)
+    {
+        data.insert(i,i*10);
+        data.PrettyPrinting();
+        std::cout << std::endl;
+    }
     
-    data.traverse();
-    data.PrettyPrinting();
+    // data.traverse();
+    // data.PrettyPrinting();
 
-    std::cout << "Searching" << std::endl;
-    std::cout << "================" << std::endl;
-    std::cout << data.search(809) << std::endl;
+    // std::cout << "Searching" << std::endl;
+    // std::cout << "================" << std::endl;
+    // std::cout << data.search(809) << std::endl;
 
-    data.deleteKey(193);
+    std::cout << "After Deleting 1, 2 and 5" << std::endl;
+    data.deleteKey(1);
+    data.deleteKey(2);
+    data.deleteKey(5);
     data.PrettyPrinting();
 
     return 0;
